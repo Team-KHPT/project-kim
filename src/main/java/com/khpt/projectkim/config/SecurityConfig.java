8a95f76875ce6f822adc5d384cafef4db006d9dd
siwonKH <kanghyunkim313@gmail.com>
2023-06-28 08:39:17 +0900

    Feat: add dynamic login success redirect

--- src/main/java/com/khpt/projectkim/config/SecurityConfig.java
+++ src/main/java/com/khpt/projectkim/config/SecurityConfig.java
@@ -7,7 +7,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @EnableWebSecurity
 @Configuration
@@ -26,14 +33,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/**", "/login/**", "/error").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .successHandler(new LoginSuccessHandler())
-                .and()
                 .logout().logoutSuccessUrl("/chat")
                 .and()
                 .exceptionHandling()
                 .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 .and()
-                .oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);
+                .oauth2Login()
+                .userInfoEndpoint().userService(customOAuth2UserService)
+                .and()
+                .successHandler(new SimpleUrlAuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        String redirectUrl = request.getSession().getAttribute("redirect").toString();
+                        getRedirectStrategy().sendRedirect(request, response, redirectUrl);
+                    }
+                });
     }
 }
