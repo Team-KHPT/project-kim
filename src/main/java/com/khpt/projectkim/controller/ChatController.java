30891d9ff179b42fc17a7770b305e2258b9fb09f
siwonKH <kanghyunkim313@gmail.com>
2023-06-27 21:53:15 +0900

    Test

--- src/main/java/com/khpt/projectkim/controller/ChatController.java
+++ src/main/java/com/khpt/projectkim/controller/ChatController.java
@@ -2,7 +2,7 @@ package com.khpt.projectkim.controller;
 
 import com.khpt.projectkim.entity.User;
 import com.khpt.projectkim.model.ChatData;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,14 +13,17 @@ import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
-@RequiredArgsConstructor
 public class ChatController {
-    private final HttpSession httpSession;
 
+    @Value("${spring.datasource.url}")
+    private String debug;
     @GetMapping("/chat")
-    public String chat(Model model) {
-        User user = (User) httpSession.getAttribute("user");
+    public String chat(HttpSession session, Model model) {
+        System.out.println("\n\n\n\nDEBUG!!" + debug + "\n\n\n");
+
+        User user = (User) session.getAttribute("user");
         if (user == null){
+            model.addAttribute("image", "/icons/black.png");
             return "chat";
         }
         model.addAttribute("name", user.getLogin());
@@ -30,7 +33,7 @@ public class ChatController {
     }
 
     @PostMapping("/chat")
-    public ChatData sendChat(@RequestParam List<ChatData> chatDataList) {
+    public ChatData sendChat(HttpSession session, @RequestParam List<ChatData> chatDataList) {
         // TODO API 요청으로 responseChat 가져오기
         ChatData responseChat = new ChatData("assistant", "안녕하세요. 무엇을 도와드릴까요?");
         return responseChat;
