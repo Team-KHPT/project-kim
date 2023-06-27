81077a9b6ed180fe51dcfc27d52b6fe8036a38a9
siwonKH <kanghyunkim313@gmail.com>
2023-06-27 21:31:12 +0900

    Update deploy shell script

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ git pull
 
 pm2 stop project-kim || true
 
-pm2 start --name project-kim java -Xmx300m -- -jar build/libs/project-kim-0.0.1-SNAPSHOT.jar
+pm2 start --name project-kim java -Xmx1G -jar build/libs/project-kim-0.0.1-SNAPSHOT.jar
 
 exit 0
