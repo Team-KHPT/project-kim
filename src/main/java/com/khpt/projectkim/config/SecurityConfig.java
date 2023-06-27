b6aebb3a07016f0e0ffd398b82c25d2632ca4d42
siwonKH <kanghyunkim313@gmail.com>
2023-06-27 16:26:44 +0900

    Refactor SecurityConfig

--- src/main/java/com/khpt/projectkim/config/SecurityConfig.java
+++ src/main/java/com/khpt/projectkim/config/SecurityConfig.java
@@ -1,23 +1,13 @@
 package com.khpt.projectkim.config;
 
 import com.khpt.projectkim.service.CustomOAuth2UserService;
-import com.khpt.projectkim.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collections;
-import java.util.Map;
 
 @EnableWebSecurity
 @Configuration
