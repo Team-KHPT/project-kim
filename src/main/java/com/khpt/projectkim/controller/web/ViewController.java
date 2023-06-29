914bf8afa9d292031f62b70e92f0496d44eb16e3
siwonKH <kanghyunkim313@gmail.com>
2023-06-29 19:50:10 +0900

    Some refactors

--- src/main/java/com/khpt/projectkim/controller/web/ViewController.java
+++ src/main/java/com/khpt/projectkim/controller/web/ViewController.java
@@ -1,23 +1,10 @@
 package com.khpt.projectkim.controller.web;
 
-import com.khpt.projectkim.entity.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.HttpSession;
-import java.util.Date;
 
 @Controller
 public class ViewController {
-    @GetMapping(value = {"/", "/index"})
-    public String index(HttpSession session, Model model) {
-        User user = (User) session.getAttribute("user");
-        if(user != null){
-            model.addAttribute("name", user.getLogin());
-        }
-        return "index";
-    }
 
     @GetMapping("chatTest")
     public String test() {
