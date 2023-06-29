1e2806985933f033aa59147cb69fa05b58404f0f
김동혁 <kdh1022255@gmail.com>
2023-06-29 22:02:00 +0900

    Update ChatRestController

--- src/main/java/com/khpt/projectkim/controller/api/ChatRestController.java
+++ src/main/java/com/khpt/projectkim/controller/api/ChatRestController.java
@@ -1,26 +1,67 @@
 package com.khpt.projectkim.controller.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.khpt.projectkim.dto.ChatData;
-import org.springframework.http.HttpStatus;
+import com.khpt.projectkim.entity.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
 @RequestMapping("/chat")
 public class ChatRestController {
 
-
     // (프론트에서 채팅 보내기 클릭 하면 이 함수에 모든 채팅 기록을 줌)
     @PostMapping
     public ChatData sendChat(HttpSession session, HttpServletResponse response, @RequestBody List<ChatData> chatDataList) {
-        if (session.getAttribute("user") == null) {
-            System.out.println("Send chat failed. No session");
-            response.setStatus(HttpStatus.BAD_REQUEST.value());
-            return null;
+//        if (session.getAttribute("user") == null) {
+//            System.out.println("Send chat failed. No session");
+//            response.setStatus(HttpStatus.BAD_REQUEST.value());
+//            return null;
+//        }
+        ChatData lastItem = chatDataList.get(chatDataList.size() - 1);
+        System.out.println(lastItem);
+
+        try {
+            System.setProperty("https.protocols", "TLSv1.2");
+
+            // List를 JSON 형태로 변환
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json = objectMapper.writeValueAsString(chatDataList);
+            System.out.println(json);
+
+            // RestTemplate 인스턴스 생성
+            RestTemplate restTemplate = new RestTemplate();
+
+            // ChatData 객체 생성
+            ChatData chatData = ChatData.builder()
+                    .role("assistant")
+                    .content("김비서 임다")
+                    .build();
+
+            // 요청 헤더 설정
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            // 요청 엔티티 생성
+            HttpEntity<ChatData> requestEntity = new HttpEntity<>(chatData, headers);
+
+            // POST 요청 보내기
+            String url = "https://localhost:8090/result";
+            ResponseEntity<String> res = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
+
+            // 응답 데이터 가져오기
+            String responseBody = res.getBody();
+            System.out.println("Response: " + responseBody);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
         // ChatData 클래스는 채팅 하나를 의미함
@@ -29,7 +70,7 @@ public class ChatRestController {
         // 물론 이 리스트의 마지막 아이템에 유저가 새로 입력한 채팅이 있음
         // TODO chatDataList의 젤 마지막 아이템을 사용자 레포지토리 채팅 기록에 추가
 
-        // TODO chatDataList의 젤 마지막 아이템을 JSON으로 변환
+        // TODO chatDataList JSON으로 변환
 
         // TODO 변환된 JSON을 API에 보내서 responseChat(GPT 응답) 가져오기
 
