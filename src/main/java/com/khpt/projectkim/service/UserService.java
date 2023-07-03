e56fc9e3c1300993b6a5dc92d53a31627e43cd13
siwonKH <kanghyunkim313@gmail.com>
2023-07-03 11:34:20 +0900

    Update UserService get, set results

--- src/main/java/com/khpt/projectkim/service/UserService.java
+++ src/main/java/com/khpt/projectkim/service/UserService.java
@@ -1,10 +1,18 @@
 package com.khpt.projectkim.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.khpt.projectkim.dto.UserPrevData;
+import com.khpt.projectkim.entity.Result;
 import com.khpt.projectkim.entity.User;
 import com.khpt.projectkim.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +22,94 @@ public class UserService {
     public User getUserByStringId(String id) {
         return userRepository.findById(Long.parseLong(id)).orElseThrow();
     }
+
+
+    public void setUserResults(String id, Map<String, List<Map<String, Object>>> results) {
+        User user = getUserByStringId(id);
+
+        List<Result> saveResults = new ArrayList<>();
+        for (Map<String, Object> r : results.get("jobs")) {
+            Result result = new Result();
+            result.setCompany(r.get("company").toString());
+            result.setEducation(r.get("education").toString());
+            result.setRegion(r.get("location").toString());
+            result.setSalary(r.get("salary").toString());
+            result.setTitle(r.get("title").toString());
+            result.setType(r.get("type").toString());
+            result.setUrl(r.get("url").toString());
+            result.setUser(user);
+            saveResults.add(result);
+        }
+
+        user.setResults(saveResults);
+        userRepository.save(user);
+    }
+
+    public String getUserResultsAsString(String id) {
+        try {
+            // Get the user
+            User user = getUserByStringId(id);
+
+            // Get the user's results
+            List<Result> results = user.getResults();
+
+            // Convert the results to a list of maps
+            List<Map<String, Object>> resultMaps = results.stream().map(result -> {
+                Map<String, Object> map = new HashMap<>();
+                map.put("company", result.getCompany());
+                map.put("education", result.getEducation());
+                map.put("location", result.getRegion());
+                map.put("salary", result.getSalary());
+                map.put("title", result.getTitle());
+                map.put("type", result.getType());
+                map.put("url", result.getUrl());
+                return map;
+            }).collect(Collectors.toList());
+
+            // Create a map for the final result
+            Map<String, List<Map<String, Object>>> finalResult = new HashMap<>();
+            finalResult.put("jobs", resultMaps);
+
+            // Convert the map to JSON
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            // Return the JSON string
+            return objectMapper.writeValueAsString(finalResult);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Map<String, List<Map<String, Object>>> getUserResultsAsMap(String id) {
+        // Get the user
+        User user = getUserByStringId(id);
+
+        // Get the user's results
+        List<Result> results = user.getResults();
+
+        // Convert the results to a list of maps
+        List<Map<String, Object>> resultMaps = results.stream().map(result -> {
+            Map<String, Object> map = new HashMap<>();
+            map.put("company", result.getCompany());
+            map.put("education", result.getEducation());
+            map.put("location", result.getRegion());
+            map.put("salary", result.getSalary());
+            map.put("title", result.getTitle());
+            map.put("type", result.getType());
+            map.put("url", result.getUrl());
+            return map;
+        }).collect(Collectors.toList());
+
+        // Create a map for the final result
+        Map<String, List<Map<String, Object>>> finalResult = new HashMap<>();
+        finalResult.put("jobs", resultMaps);
+
+        // Return the final map
+        return finalResult;
+    }
+
+
 
     public void setUserPrevData(String id, UserPrevData userPrevData) {
         User user = getUserByStringId(id);
