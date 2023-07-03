8cb35c81ed216bc995a91c1d3f3a97d2febe18a0
siwonKH <kanghyunkim313@gmail.com>
2023-07-03 14:28:13 +0900

    Fix: typo

--- src/main/java/com/khpt/projectkim/controller/api/ResultRestController.java
+++ src/main/java/com/khpt/projectkim/controller/api/ResultRestController.java
@@ -1,35 +1,27 @@
 package com.khpt.projectkim.controller.api;
 
 import com.khpt.projectkim.dto.ResultDto;
-import com.khpt.projectkim.service.ApiRequestService;
 import com.khpt.projectkim.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class ResultRestController {
 
-    private final ApiRequestService apiRequestService;
-
     private final UserService userService;
 
     @GetMapping("/result")
-    public List<ResultDto> getResult(HttpSession session, HttpServletResponse response) {
+    public List<ResultDto> getResult(HttpSession session, HttpServletResponse response) throws IOException {
         if (session.getAttribute("user") == null) {
-            System.out.println("Get chat failed. No session");
-            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            System.out.println("Get result failed. No session");
+            response.sendRedirect("/");
             return null;
         }
         String userId = session.getAttribute("user").toString();
