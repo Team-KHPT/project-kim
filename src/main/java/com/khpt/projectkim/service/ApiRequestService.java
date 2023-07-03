451c23244f6acdecbe2b54af3d0c55e9fc7b3bf7
siwonKH <kanghyunkim313@gmail.com>
2023-07-03 11:37:24 +0900

    Refactor: remove commented code

--- src/main/java/com/khpt/projectkim/service/ApiRequestService.java
+++ src/main/java/com/khpt/projectkim/service/ApiRequestService.java
@@ -18,35 +18,6 @@ import static com.khpt.projectkim.service.SimplifyJsonService.simplifyJobs;
 public class ApiRequestService {
 
     public String getApiResponseAsString(String url, Map<String, String> params) throws JsonProcessingException {
-//        RestTemplate restTemplate = new RestTemplate();
-//        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url);
-//
-//        // Add the parameters to the request
-//        for (Map.Entry<String, String> entry : params.entrySet()) {
-//            builder.queryParam(entry.getKey(), entry.getValue());
-//        }
-//
-////        HttpHeaders headers = new HttpHeaders();
-////        headers.add("Accept", "application/json");
-////        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
-////
-//////        ResponseEntity<String> response = restTemplate.getForEntity(builder.toUriString(), String.class, headers);
-////        ResponseEntity<String> response = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, httpEntity, String.class);
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
-//        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-//
-//        ResponseEntity<String> response = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, entity, String.class);
-//
-//        System.out.println("url");
-//        System.out.println(builder.toUriString());
-//        System.out.println(response);
-//
-//        if (response.getStatusCodeValue() == 200) {
-//            return response.getBody();
-//        } else {
-//            throw new RuntimeException("Failed : HTTP error code : " + response.getStatusCodeValue());
-//        }
         RestTemplate restTemplate = new RestTemplate();
 
         // Set request headers
