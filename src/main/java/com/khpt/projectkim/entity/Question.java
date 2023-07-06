236faadbb68d738af30eca45f5664642c277a2c5
김동혁 <kdh1022255@gmail.com>
2023-07-06 15:47:55 +0900

    create Question

---
+++ src/main/java/com/khpt/projectkim/entity/Question.java
@@ -0,0 +1,24 @@
+package com.khpt.projectkim.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String question;
+}
