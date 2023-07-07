526b6cc584086621327b5b2e0b37f236de3e35b4
siwonKH <kanghyunkim313@gmail.com>
2023-07-07 22:17:32 +0900

    Add: Example entity assisting dto

---
+++ src/main/java/com/khpt/projectkim/dto/ExampleChat.java
@@ -0,0 +1,15 @@
+package com.khpt.projectkim.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExampleChat {
+    private String id;
+    private String chat;
+}
---
+++ src/main/java/com/khpt/projectkim/dto/ExampleResult.java
@@ -0,0 +1,15 @@
+package com.khpt.projectkim.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExampleResult {
+    private String response;
+    private String result;
+}
