0a75a50f65249eed9946e9c1280053a4bc3a484b
siwonKH <kanghyunkim313@gmail.com>
2023-07-07 22:18:07 +0900

    Update: add method for examples

--- src/main/java/com/khpt/projectkim/service/QuestionService.java
+++ src/main/java/com/khpt/projectkim/service/QuestionService.java
@@ -1,6 +1,8 @@
 package com.khpt.projectkim.service;
 
+import com.khpt.projectkim.entity.Example;
 import com.khpt.projectkim.entity.Question;
+import com.khpt.projectkim.repository.ExampleRepository;
 import com.khpt.projectkim.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,9 +15,21 @@ public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
+    private final ExampleRepository exampleRepository;
+
     public List<String> getRandomQuestions() {
         List<String> questionList = questionRepository.findRandomEntities();
         System.out.println(questionList);
         return questionList;
+    }
+
+    public List<Example> getRandomExamples() {
+        List<Example> exampleList = exampleRepository.findRandomEntities();
+        System.out.println(exampleList);
+        return exampleList;
+    }
+
+    public Example getExampleByStringId(String id) {
+        return exampleRepository.findById(Long.parseLong(id)).orElse(new Example());
     }
 }
