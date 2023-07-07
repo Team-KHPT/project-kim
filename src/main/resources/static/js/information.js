f7993557c66f02237ecd81bc697cbe8e10c7da21
siwonKH <kanghyunkim313@gmail.com>
2023-07-07 22:21:20 +0900

    Update: reload after change prev data

--- src/main/resources/static/js/information.js
+++ src/main/resources/static/js/information.js
@@ -164,6 +164,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 } else {
                     console.log('error')
                 }
+                location.reload()
             })
     });
 });
