10f8cca1cb888bcb2ca2f41ba91063eeb5d0f68a
siwonKH <kanghyunkim313@gmail.com>
2023-07-12 03:22:33 +0900

    Update: add config for mobile

--- src/main/java/com/khpt/projectkim/config/AppConfig.java
+++ src/main/java/com/khpt/projectkim/config/AppConfig.java
@@ -1,14 +1,40 @@
 package com.khpt.projectkim.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mobile.device.DeviceHandlerMethodArgumentResolver;
+import org.springframework.mobile.device.DeviceResolverHandlerInterceptor;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
 
+    @Bean
+    public DeviceResolverHandlerInterceptor
+    deviceResolverHandlerInterceptor() {
+        return new DeviceResolverHandlerInterceptor();
+    }
+
+    @Bean
+    public DeviceHandlerMethodArgumentResolver
+    deviceHandlerMethodArgumentResolver() {
+        return new DeviceHandlerMethodArgumentResolver();
+    }
+
+    @Override
+    public void addArgumentResolvers(
+            List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(deviceHandlerMethodArgumentResolver());
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(deviceResolverHandlerInterceptor());
         registry.addInterceptor(new LogInterceptor()).addPathPatterns("/**");
     }
 
