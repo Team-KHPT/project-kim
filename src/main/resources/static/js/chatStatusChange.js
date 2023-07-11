2bdd986852516189e3f0a6213bcf9c616076e6d8
김동혁 <kdh1022255@gmail.com>
2023-07-11 16:57:49 +0900

    Update chatStatusChange

--- src/main/resources/static/js/chatStatusChange.js
+++ src/main/resources/static/js/chatStatusChange.js
@@ -19,8 +19,23 @@ const deleteBtns = document.querySelectorAll(".deleteButton")
 deleteBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         if (confirm('해당 채팅을 삭제하시겠습니까?')) {
+            fetch('/chat/delete', {
+                method: 'DELETE'
+            })
+                .then(response => {
+                    if (response.ok) {
+                        console.log('마지막 인자 두 개 삭제 성공');
+                    } else {
+                        console.log('마지막 인자 두 개 삭제 실패');
+                    }
+                })
+                .catch(error => {
+                    console.error('요청 중 오류 발생:', error);
+                });
+
             const parentDiv = btn.parentElement.parentElement.parentElement
             const nextSibling = btn.parentElement.parentElement.parentElement.nextElementSibling;
+
             parentDiv.remove();
             nextSibling.remove();
         }
