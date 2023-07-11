e148fd2aa9027e9b580bb4112a8592050b296058
siwonKH <kanghyunkim313@gmail.com>
2023-07-12 03:24:58 +0900

    Update: handles info modal is null

--- src/main/resources/static/js/question.js
+++ src/main/resources/static/js/question.js
@@ -20,18 +20,21 @@ async function getExamples() {
         .catch(() => console.log(''))
 
     if (dataArray != null) {
-        const chatInput = document.getElementById("chat-input")
         const infoModal = document.getElementById("info-modal")
+        if (infoModal != null) {
+            infoModal.setAttribute("disabled", "")
+            infoModal.classList.add("cursor-not-allowed")
+        }
+
+        const chatInput = document.getElementById("chat-input")
         chatInput.setAttribute("disabled", "")
-        infoModal.setAttribute("disabled", "")
         chatInput.classList.add("cursor-not-allowed")
-        infoModal.classList.add("cursor-not-allowed")
 
         const exampleElements = document.querySelector("#examples div div")
 
         for (data of dataArray) {
             let exampleElem = document.createElement('a');
-            exampleElem.className = 'w-fit bg-white text-violet-600 px-4 py-2 rounded-full border border-violet-300 flex items-center justify-center mt-3 cursor-pointer transition-colors hover:bg-stone-100';
+            exampleElem.className = 'w-fit bg-white text-sm md:text-base lg:text-base text-violet-600 px-4 py-2 rounded-3xl border border-violet-300 flex items-center justify-center mt-3 cursor-pointer transition-colors hover:bg-stone-100';
             exampleElem.textContent = data.chat
             exampleElem.setAttribute("chat-id", data.id)
 
@@ -59,7 +62,7 @@ async function getExamples() {
                         chats.appendChild(assistantChatItem)
 
                         setTimeout(() => {
-                            chats.appendChild(makeAssistantChatItem(`<div class="flex flex-col space-y-2"><div>위 답변은 예시 답변입니다. 모든 기능을 이용하기 위해선 <a class="text-violet-500" href="/login?redirect=${location.href}">로그인</a>을 해주세요.</div><a href="/chat" class="w-fit px-4 py-2 bg-violet-200 rounded-lg text-sm hover:bg-violet-300">다시하기</a></div>`, false))
+                            chats.appendChild(makeAssistantChatItem(`<div class="flex flex-col space-y-2"><div>위 답변은 예시 답변입니다. 모든 기능을 이용하기 위해선 <a class="text-violet-500" href="/login?redirect=${location.href}">로그인</a>을 해주세요.</div><a href="/" class="w-fit px-4 py-2 bg-violet-200 rounded-lg text-sm hover:bg-violet-300">다시하기</a></div>`, false))
                         }, 1500)
 
                         await setData(JSON.parse(data.result))
