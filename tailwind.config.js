838255b92d72d3ec7c4cb49defc2224d2d596b80
siwonKH <kanghyunkim313@gmail.com>
2023-07-11 20:54:23 +0900

    Update: remove mustache

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js,mustache}",
+    "./src/**/*.{html,js}",
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
