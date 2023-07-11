d8449e7d9dce1115dfef34ca552c4af2490a029a
김동혁 <kdh1022255@gmail.com>
2023-07-11 16:57:16 +0900

    Update chatInput

--- src/main/resources/static/js/chatInput.js
+++ src/main/resources/static/js/chatInput.js
@@ -161,6 +161,10 @@ function sendMessage(inputValue) {
 }
 
 function makeUserChatItem(chat) {
+    const delBtn = document.querySelector('.deleteButton');
+    console.log(delBtn);
+    delBtn.remove();
+
     const userChatItem = document.createElement('div')
     userChatItem.classList.add('user', 'flex', 'space-x-3')
 
@@ -178,6 +182,10 @@ function makeUserChatItem(chat) {
 
     const copyButtonContainer = document.createElement("div");
     copyButtonContainer.classList.add("inline-block", "text-gray-400");
+
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('deleteButton', 'flex', 'rounded-md', 'p-1', 'hover:bg-gray-100', 'hover:text-gray-700');
+    deleteButton.innerHTML = '<svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>';
 
     const copyButton = document.createElement("button");
     copyButton.classList.add("copyButton", "flex", "rounded-md", "p-1", "hover:bg-gray-100", "hover:text-gray-700");
@@ -205,6 +213,7 @@ function makeUserChatItem(chat) {
     copyButtonIcon.appendChild(copyButtonPath2);
     copyButton.appendChild(copyButtonIcon);
     copyButtonContainer.appendChild(copyButton);
+    copyButtonContainer.appendChild(deleteButton);
 
     userText.appendChild(chatText);
     userText.appendChild(copyButtonContainer);
