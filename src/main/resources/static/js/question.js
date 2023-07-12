7edbeb0aab5fd6bca9d349c587bc19141949e64e
siwonKH <kanghyunkim313@gmail.com>
2023-07-12 15:12:11 +0900

    Fix: typo

--- src/main/resources/static/js/question.js
+++ src/main/resources/static/js/question.js
@@ -4,7 +4,7 @@ questionField.forEach(function (question) {
     question.addEventListener('click', function (event) {
         console.log(event.target.text);
         inputField.value = event.target.text;
-        inputHandler(inputField);
+        inputHandler();
     })
 });
 
